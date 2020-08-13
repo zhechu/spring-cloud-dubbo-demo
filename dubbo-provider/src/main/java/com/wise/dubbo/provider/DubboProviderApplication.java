caadef55d408336ed1a90125f785b3ae15c3fd55
lingyuwang <ling-yu-wang@qq.com>
2020-08-13 17:10:47 +0800

    refactor:SpringBoot依赖配置调整

--- dubbo-provider/src/main/java/com/wise/dubbo/provider/DubboProviderApplication.java
+++ dubbo-provider/src/main/java/com/wise/dubbo/provider/DubboProviderApplication.java
@@ -4,6 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 启动类
+ *
+ * 浏览器访问
+ * http://127.0.0.1:8091/hello
+ *
+ * @author lingyuwang
+ * @date 2020-08-13 17:06
+ * @since 1.1.0
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class DubboProviderApplication {
